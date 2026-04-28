19c0943215f3ebad5b31e3bad036f9c2fb2a477a
Tuğçe Kızıldağ <tugce.kutuk88@gmail.com>
2026-04-28 15:02:14 +0300

    Added: Hero Animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,13 @@ import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { SplitText } from 'gsap/all';
 
+import { useRef } from 'react';
+import { useMediaQuery } from 'react-responsive';
+
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   useGSAP(() => {
     const heroSplit = new SplitText('.title', { type: 'chars, words' });
     const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
@@ -36,6 +42,25 @@ const Hero = () => {
       })
       .to('.right-leaf', { y: 400 }, 0)
       .to('.left-leaf', { y: -200 }, 0);
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
   return (
     <>
@@ -56,8 +81,9 @@ const Hero = () => {
           <div className='content'>
             <div className='space-y-5 hidden md:block'>
               <p className='mt-20'>Cool. Crisp. Classic.</p>
-              <p className='subtitle'>Sip the Spirit</p>
-              <p className='subtitle'>of Summer</p>
+              <p className='subtitle'>
+                Sip the Spirit <br /> of Summer
+              </p>
             </div>
             <div className='view-cocktails'>
               <p className='subtitle'>
@@ -70,6 +96,14 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className='video absolute inset-0'>
+        <video
+          ref={videoRef}
+          src='/videos/input.mp4'
+          muted
+          playsInline
+          preload='auto'></video>
+      </div>
     </>
   );
 };
