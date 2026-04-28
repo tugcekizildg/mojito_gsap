81b0be3d7ccb2d9903821dedb8a9064a1018bc02
Tuğçe Kızıldağ <tugce.kutuk88@gmail.com>
2026-04-28 14:39:46 +0300

    Added: NavBar and Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
-    <div className='text-8xl flex-center text-indigo-300 min-h-screen'>
-      Hello, GSAP!
+    <div>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Hero />
+        <div className='h-dvh bg-black'></div>
+      </main>
     </div>
   );
 };
