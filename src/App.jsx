6f1909701281ee3106d463424aa6c8fe5c61d4db
Tuğçe Kızıldağ <tugce.kutuk88@gmail.com>
2026-04-30 23:48:58 +0300

    Added: Art Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
+import Art from './components/Art';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Hero />
         <Cocktails />
         <About />
+        <Art />
       </main>
     </div>
   );
